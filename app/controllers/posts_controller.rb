d1edf99c2eb77733ea20acb757eca8f57a40c246
madcido <fabowow@gmail.com>
2019-05-13 20:36:32 -0300

    added before filter in posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+    include SessionsHelper
+
+    before_action :is_member?, only: [:new, :create]
 
     def index
         @posts = Post.all.order("created_at DESC")   
@@ -22,6 +25,10 @@ class PostsController < ApplicationController
     def post_params
         params[:post][:user_id] = session[:id]
         params.require(:post).permit(:content, :user_id)
+    end
+
+    def is_member?
+        redirect_to root_path unless logged_in?
     end
 
 end
